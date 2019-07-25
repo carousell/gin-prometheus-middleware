713841c0ccade065a3946e0c57d6117bda9f48e9
ashraful88 <aaj.cse@gmail.com>
2019-07-25 20:04:10 +0800

    added path with method

--- prometheus.go
+++ prometheus.go
@@ -83,7 +83,7 @@ func (p *Prometheus) registerMetrics(subsystem string) {
 			Name:      "request_count",
 			Help:      "Number of request",
 		},
-		[]string{"code", "path", "method", "handler", "host", "url"},
+		[]string{"code", "path", "handler", "host", "url"},
 	)
 
 	p.reqDur = prometheus.NewHistogramVec(
@@ -93,7 +93,7 @@ func (p *Prometheus) registerMetrics(subsystem string) {
 			Help:      "request latencies",
 			Buckets:   []float64{.0005, .001, .002, 0.004, .006, 0.008, .01, 0.015, .025, 0.04, .06, .08, 0.1, 0.15, 0.2, 0.3, 0.5},
 		},
-		[]string{"code", "path", "method", "handler", "host", "url"},
+		[]string{"code", "path", "handler", "host", "url"},
 	)
 
 	prometheus.Register(p.reqCnt)
@@ -131,8 +131,8 @@ func (p *Prometheus) HandlerFunc() gin.HandlerFunc {
 			}
 			url = u.(string)
 		}
-		p.reqDur.WithLabelValues(status, c.Request.Method+"_"+c.Request.URL.Path, c.Request.Method, c.HandlerName(), c.Request.Host, url).Observe(elapsed)
-		p.reqCnt.WithLabelValues(status, c.Request.Method+"_"+c.Request.URL.Path, c.Request.Method, c.HandlerName(), c.Request.Host, url).Inc()
+		p.reqDur.WithLabelValues(status, c.Request.Method+"_"+c.Request.URL.Path, c.HandlerName(), c.Request.Host, url).Observe(elapsed)
+		p.reqCnt.WithLabelValues(status, c.Request.Method+"_"+c.Request.URL.Path, c.HandlerName(), c.Request.Host, url).Inc()
 
 	}
 }
