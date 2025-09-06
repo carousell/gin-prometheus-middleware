798dce052480f29ac9ea83332763ec124087d237
Zakir Hyder <2004937+zakir-hyder@users.noreply.github.com>
2025-09-06 21:27:00 +0600

    remove 7, 10, 15, 20 from prom bucket

--- prometheus.go
+++ prometheus.go
@@ -73,7 +73,7 @@ func (p *Prometheus) registerMetrics(subsystem string) {
 			Subsystem: subsystem,
 			Name:      "request_duration_seconds",
 			Help:      "Histogram request latencies",
-			Buckets:   []float64{0.1, 0.2, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 5, 7, 10, 15, 20},
+			Buckets:   []float64{0.1, 0.2, 0.3, 0.5, 0.75, 1, 1.5, 2, 3, 5},
 		},
 		[]string{"code", "path"},
 	)
